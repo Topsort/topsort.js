735ff57b3f25ec5216ceae38f8e16e6c8c9d18b4
barbmarcio <marcio@topsort.com>
2024-07-16 17:39:12 +0100

    feat: bundling project with tsup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { reportEvent } from "./functions/report-event";
 export { createAuction } from "./functions/create-auction";
 export * from "./interfaces/events.interface";
+export * from "./interfaces/auctions.interface";
