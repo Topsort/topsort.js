008301174eabfc8af19896c79c59bb5d810e755d
Johan Bergström <johan@topsort.com>
2024-07-17 14:25:46 +0100

    feat(ci): set up basic github actions
    Add github actions to verify PR's. These also reflects the local checks
    we do via `lefthook` with small additions that are not relevant for local tests.
    
    PR: https://github.com/Topsort/topsort.js/pull/3

--- test/validate-config.test.ts
+++ test/validate-config.test.ts
@@ -1,8 +1,7 @@
-
 import { describe, expect, it } from "bun:test";
+import AppError from "../src/lib/app-error";
 import { validateConfig } from "../src/lib/validate-config";
 import type { Config } from "../src/types/shared";
-import AppError from "../src/lib/app-error";
 
 describe("validateConfig", () => {
   it("should throw an error if apiKey is missing", () => {
