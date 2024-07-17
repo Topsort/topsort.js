02189cb65fc1750e1261df849faddac385d5b6a2
barbmarcio <marcio@topsort.com>
2024-07-17 02:01:01 +0100

    chore: removing unused files

--- tests/validate-config.test.ts
+++ tests/validate-config.test.ts
@@ -1,6 +1,8 @@
-import AppError from "../src/lib/app-error";
+
+import { describe, expect, it } from "bun:test";
 import { validateConfig } from "../src/lib/validate-config";
 import type { Config } from "../src/types/shared";
+import AppError from "../src/lib/app-error";
 
 describe("validateConfig", () => {
   it("should throw an error if apiKey is missing", () => {
