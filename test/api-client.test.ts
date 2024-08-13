42b8ae9dbda6d6ffc5ea98131a47a9a6c191a38f
barbmarcio <marcio@topsort.com>
2024-08-13 11:03:38 +0100

    feat: adds test for trailing slash

---
+++ test/api-client.test.ts
@@ -0,0 +1,33 @@
+import { afterEach, beforeAll, describe, expect, it } from "bun:test";
+import { Config, TopsortEvent } from "../src";
+import { apis, baseURL } from "../src/constants/apis.constant";
+import { mswServer, returnAuctionSuccess } from "../src/constants/handlers.constant";
+import APIClient from "../src/lib/api-client";
+
+describe("apiClient", () => {
+  beforeAll(() => mswServer.listen());
+  afterEach(() => mswServer.resetHandlers());
+
+  it("should handle custom url with trailing slash", async () => {
+    const customURL = `${baseURL}/${apis.auctions}/`;
+    const config: Config = {
+        apiKey: "apiKey",
+    }
+    returnAuctionSuccess(`${baseURL}/${apis.auctions}`);
+
+    expect(APIClient.post(customURL, {} as TopsortEvent, config)).resolves.toEqual({
+        results: [
+          {
+            resultType: "listings",
+            winners: [],
+            error: false,
+          },
+          {
+            resultType: "banners",
+            winners: [],
+            error: false,
+          },
+        ],
+      });
+  });
+});
